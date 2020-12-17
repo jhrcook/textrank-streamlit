45b0b59f67df24740eeb59bad06bf103897bb4ab
Josh Cook <joshuacook0023@gmail.com>
2020-12-17 15:27:46 -0500

    feat: text summarizer app

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ EXPOSE 8501
 WORKDIR /app
 COPY requirements.txt ./requirements.txt
 RUN pip3 install -r requirements.txt
-RUN python -m spacy download en_core_web_sm
 COPY . .
 CMD streamlit run --server.port $PORT app.py
--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-import spacy
+from summa import summarizer
 import streamlit as st
 
-nlp = spacy.load("en_core_web_sm")
-
-
-def return_NER(value):
-    doc = nlp(value)
-    return [(X.text, X.label_) for X in doc.ents]
-
-
 # Add title on the page
-st.title("Spacy - Named Entity Recognition")
+st.title("Text summarization")
 
 # Ask user for input text
-input_sent = st.text_input("Input Sentence", "Your input sentence goes here")
+input_sent = st.text_area("Input Text", "", height=400)
+
+ratio = st.slider(
+    "Summarization fraction", min_value=0.0, max_value=1.0, value=0.2, step=0.01
+)
 
 # Display named entities
-for res in return_NER(input_sent):
-    st.write(res[0], "-->", res[1])
+summarized_text = summarizer.summarize(
+    input_sent, ratio=ratio, language="english", split=True, scores=True
+)
+
+for sentence, score in summarized_text:
+    st.write(sentence)
